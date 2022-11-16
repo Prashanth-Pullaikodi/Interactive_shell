327753bbc850c05a12252c7934247629a0f5b99d
Prashanth Pullaikodi <30933466+Prashanth-Pullaikodi@users.noreply.github.com>
2022-11-16 14:59:35 +0100

    Update formatting

--- interactive_swarm_menu.sh
+++ interactive_swarm_menu.sh
@@ -19,11 +19,18 @@ fail() { echo "Wrong option." exit 1; }
 
 connect_shell() {
                 read -p "Enter service name to connect  : " service_name
-                read -p "Enter command to execute on container : "  command
+                service_name=${service_name:-$}
+                read -p "Enter command to execute on container [$(blueprint DEFAULT) $(yellowprint sh)] : "  command
                 command=${command:-sh}
                 uname=`whoami`
-                read -p "Enter your username to connect swarm worker host [default $uname]: " username
+                read -p "Enter your username to connect swarm worker host [$(blueprint DEFAULT)  $(yellowprint $uname) ]: " username
                 username=${username:-$uname}
+                echo -e "\n"
+                sudo docker service ls|grep $service_name
+                echo -e "\n"
+                read -p "Enter $(yellowprint Actual) $(yellowprint service_name) to connect  : " service_name
+                echo -e "\n"
+                sudo docker service ps  $service_name -f 'desired-state=running'
                 echo -e "\n"
                 ServiceId=`sudo docker service ps $service_name -f 'desired-state=running' --format '{{.ID}}' | head -n1`
                 service_node=`sudo docker service ps $service_name -f 'desired-state=running' --format '{{.Node}}' | head -n1`
@@ -31,103 +38,121 @@ connect_shell() {
                 real_service_id_tmp=`ssh -o LogLevel=QUIET -t $username@$service_node "sudo docker ps | grep $ServiceId | cut -f1 -d' '"`
                 real_service_id=`tr -d '\r' <<< $real_service_id_tmp`
                 echo -ne "Service $(redprint $service_name) is running on $(redprint $service_node) with real Task id: $(redprint $real_service_id) \n"
-                echo -ne "Connecting service ID  $(redprint $real_service_id) on $(redprint $service_node) with username $(redprint $username)"
+                echo -ne "Connecting service ID  $(redprint $real_service_id) on $(redprint $service_node) with username $(redprint $username)\n"
+                echo -ne "Type $(yellowprint exit) to return $(yellowprint Menu Options)"
                 echo -e "\n"
                 ssh -o LogLevel=QUIET -t $username@$service_node "sudo docker exec -ti $real_service_id $command"
 }
 
 list_services(){
-        read -p "Enter service name to search : " service_name
-        service_name=${service_name:-$}
-        echo -e "\n"
-        sudo docker service ls  |egrep "$service_name"
-        echo -e "\n"
+                read -p "Enter service name to search : " service_name
+                service_name=${service_name:-$}
+                echo -e "\n"
+                sudo docker service ls  |egrep "$service_name"
+                echo -e "\n"
 }
 
 task_state(){
-        read -p "Enter service name to search : " service_name
-        echo -e "\n"
-        sudo docker service ps  $service_name -f 'desired-state=running'
-        echo -e "\n"
+                read -p "Enter service name: " service_name
+                service_name=${service_name:-$}
+                echo -e "\n"
+                sudo docker service ls|grep $service_name
+                echo -e "\n"
+                read -p "Enter $(yellowprint Actual) $(yellowprint service_name) to display STATE  : " service_name
+                echo -e "\n"
+                sudo docker service ps  $service_name -f 'desired-state=running'
+                echo -e "\n"
 }
 
 tail_logs(){
-        read -p "Enter service name to tail logs : " service_name
-        read -p "Show logs since timestamp (e.g. 1h for 1 hour) or relative (e.g. 42m for 42 minutes), Default 1m :  " since
-        since=${since:-1m}
-        echo -e "\n"
-        sudo docker service logs -f  $service_name --since $since
-        echo -e "\n"
+                read -p "Enter service name : " service_name
+                service_name=${service_name:-$}
+                read -p "Show logs since timestamp e.g. 1h for 1 hours OR 42m for 42 minutes) [$(yellowprint DEFAULT) $(yellowprint 1m)] :  " since
+                since=${since:-1m}
+                echo -e "\n"
+                sudo docker service ls|grep $service_name
+                echo -e "\n"
+                read -p "Enter $(yellowprint Actual) $(yellowprint service_name) to display logs : " service_name
+                echo -e "\n"
+                sudo docker service logs -f  $service_name --since $since
+                echo -e "\n"
 }
 inspect_service(){
-        read -p "Enter service name to Inspect : " service_name
-        read -p "Enter value to filter/grep : " search_value
-        search_value=${search_value:-$}
-        echo -e "\n"
-        echo -e "-----------------------------------------------"
-        echo "docker service inspect  $service_name |egrep $search_value"
-        sudo docker service inspect  $service_name |egrep $search_value
-        echo -e "-----------------------------------------------"
-        echo -e "\n"
+                read -p "Enter service name : " service_name
+                service_name=${service_name:-$}
+                echo -e "\n"
+                sudo docker service ls|grep $service_name
+                echo -e "\n"
+                read -p "Enter $(yellowprint Actual) $(yellowprint service_name) to inspect : " service_name
+                echo -e "\n"
+        	read -p "Enter value to filter/grep : " search_value
+        	search_value=${search_value:-$}
+        	echo -e "\n"
+        	echo -e "-----------------------------------------------"
+        	echo "docker service inspect  $service_name |egrep -i  $search_value"
+        	 sudo docker service inspect  $service_name |egrep -i $search_value
+        	echo -e "-----------------------------------------------"
+        	echo -e "\n"
 }
 
 
 docker_config(){
-        read -p "Enter config name to search : " config_name
-        config_name=${config_name:-$}
-        echo -e "\n"
-        sudo docker config ls  |egrep "$config_name"
-        echo -e "\n"
+        	read -p "Enter config name to search : " config_name
+        	config_name=${config_name:-$}
+        	echo -e "\n"
+        	sudo docker config ls  |egrep -i "$config_name"
+        	echo -e "\n"
 }
 
 docker_secret(){
-        read -p "Enter docker_secret name to search : " secret_name
-        secret_name=${secret_name:-$}
-        echo -e "\n"
-        sudo docker secret ls  |egrep "$secret_name"
-        echo -e "\n"
+        	read -p "Enter docker_secret name to search : " secret_name
+       		secret_name=${secret_name:-$}
+        	echo -e "\n"
+        	sudo docker secret ls  |egrep -i "$secret_name"
+        	echo -e "\n"
 }
 
 
 connect_worker() {
-        sudo docker node ls
-        echo -e "\n"
-        uname=`whoami`
-        read -p "Enter HOSTNAME to connect : " worker_name
-        read -p "Enter your USERNAME to connect swarm worker host [default $uname]: " username
-        username=${username:-$uname}
-        read -p "Enter command to execute on container : [Default bash] "  command
-        command=${command:-bash}
-        echo "Connecting to WROKER NODW $(redprint $worker_name) with USERNAME $(redprint $username)"
-        echo -e "\n"
-        ssh -o LogLevel=QUIET  -tt -o StrictHostKeyChecking=no $username@$worker_name "$command"
+        	sudo docker node ls
+        	echo -e "\n"
+        	uname=`whoami`
+        	read -p "Enter $(redprint HOSTNAME) to connect : " worker_name
+        	read -p "Enter your $(redprint USERNAME) to connect swarm worker host [$(redprint DEFAULT) $(redprint $uname)]: " username
+        	username=${username:-$uname}
+        	read -p "Enter command to execute on docker host : [$(redprint DEFAULT)  $(redprint bash)] "  command
+        	command=${command:-bash}
+        	echo -ne "Connecting to WROKER NODE $(redprint $worker_name) with USERNAME $(redprint $username)\n"
+                echo -ne "Type $(yellowprint exit) to return $(yellowprint Menu Options)"
+        	echo -e "\n"
+        	ssh -o LogLevel=QUIET  -tt -o StrictHostKeyChecking=no $username@$worker_name "$command"
 }
 
 
 worker_inspect() {
-        sudo docker node ls
-        echo -e "\n"
-        read -p "Enter wroker name to Inspect : " worker_name
-        read -p "Enter filter value : " search_value
-        search_value=${search_value:-$}
-        echo -e "\n"
-        echo -e "-----------------------------------------------"
-        echo "sudo docker node inspect  $worker_name |egrep $search_value"
-        sudo docker node inspect  $worker_name |egrep $search_value
-        echo -e "-----------------------------------------------"
-        echo -e "\n"
+        	sudo docker node ls
+        	echo -e "\n"
+        	read -p "Enter wroker node  name to inspect : " worker_name
+        	read -p "Enter filter value : " search_value
+        	search_value=${search_value:-$}
+        	echo -e "\n"
+        	echo -e "-----------------------------------------------"
+        	echo "sudo docker node inspect  $worker_name |egrep -i $search_value"
+        	sudo docker node inspect  $worker_name |egrep -i $search_value
+        	echo -e "-----------------------------------------------"
+        	echo -e "\n"
 }
 
 SwarmServiceCommands() {
 echo -ne "
-$(blueprint 'Swarm Service Commands')
-$(greenprint '1)') List Swarm Services
-$(greenprint '2)') Swarm Service TaskState
-$(greenprint '3)') Tail Service Logs
-$(greenprint '4)') Inspect Swarm Service
-$(greenprint '5)') Connect To Shell
-$(magentaprint '6)') Go Back to Main Menu
-$(redprint '0)') Exit
+ $(blueprint 'Swarm Service Commands')
+ $(greenprint '1)') List Swarm Services
+ $(greenprint '2)') Swarm Service TaskState
+ $(greenprint '3)') Tail Service Logs
+ $(greenprint '4)') Inspect Swarm Service
+ $(greenprint '5)') Connect To Shell
+ $(magentaprint '6)') Go Back to Main Menu
+ $(redprint '0)') Exit
 Choose an option:  "
     read -r ans
     case $ans in
@@ -167,15 +192,15 @@ Choose an option:  "
 
 
 SwarmClusterCommands() {
-echo -ne "
-$(blueprint 'SwarmClusterCommands')
-$(greenprint '1)') List Swarm Nodes
-$(greenprint '2)') List Docker Configs
-$(greenprint '3)') List Docker Secret
-$(greenprint '4)') Inspect Swarm Nodes
-$(greenprint '5)') Connect To Wroker Node
-$(magentaprint '6)') Go Back to Main Menu
-$(redprint '0)') Exit
+   echo -ne "
+ $(blueprint 'SwarmClusterCommands')
+ $(greenprint '1)') List Swarm Nodes
+ $(greenprint '2)') List Docker Configs
+ $(greenprint '3)') List Docker Secret
+ $(greenprint '4)') Inspect Swarm Nodes
+ $(greenprint '5)') Connect To Wroker Node
+ $(magentaprint '6)') Go Back to Main Menu
+ $(redprint '0)') Exit
 Choose an option:  "
     read -r ans
     case $ans in
